355c7f80cb86066ab3bf102d895e3cc6e3710f34
Guilherme Torres <guilhermetorres97@gmail.com>
2018-07-04 23:22:03 -0300

    init qvalues zero

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use std::fs::File;
 use std::error::Error;
 use std::env;
 use std::io::{BufRead, BufReader};
+mod qlearning;
 
 fn main()
 {
@@ -23,11 +24,35 @@ fn main()
         .take(2)
         .map(|n| n.parse().unwrap())
         .collect();
-    let mut map: Vec<Vec<char>> = reader.lines()
+
+    // get map
+    let map: Vec<Vec<char>> = reader.lines()
         .take(sizes[0])
         .map(|l| l.unwrap().chars()
              .take(sizes[1])
              .collect())
         .collect();
 
+    // init q-values
+    let mut qValues: Vec<Vec<Vec<f32>>> = vec![];
+    for i in 0..sizes[0]
+    {
+        qValues.push(vec![]);
+        for j in 0..sizes[1]
+        {
+            qValues[i].push(vec![]);
+            for k in 0..4
+            {
+                qValues[i][j].push(0.0);
+            }
+        }
+    }
+
+    // iterate q-learning n times
+    let mut posX = 0;
+    let mut posY = 0;
+    for i in 0..iterations
+    {
+        qlearning::learn(&map, &mut qValues, &mut posX, &mut posY);
+    }
 }
---
+++ src/qlearning.rs
@@ -0,0 +1,3 @@
+pub fn learn(map: &Vec<Vec<char>>, qValues: &Vec<Vec<Vec<f32>>>, posX: &i16, posY: &i16)
+{
+}
